4522520152eb6d9bd1831e9b7b03a265c1bb401f
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-06-02 23:52:38 +0300

    feat(about-hero): add AboutHero component with HTML, CSS, and test files
    - Created AboutHeroComponent with corresponding HTML and CSS for layout and styling.
    - Added a test suite for AboutHeroComponent to ensure it initializes correctly.

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AboutHeroComponent } from './component/about-hero/about-hero.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [AboutHeroComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
---
+++ src/app/features/about/component/about-hero/about-hero.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-hero',
+  imports: [],
+  templateUrl: './about-hero.component.html',
+  styleUrl: './about-hero.component.css',
+})
+export class AboutHeroComponent {}
