fb65ac7ea51bd5139e5921e93ef3c2677e893801
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-06-02 07:00:42 +0300

    feat: add destinations component with slider functionality
    - Implemented destinations component with HTML structure for destination cards.
    - Added CSS for slider animation and card styling.
    - Created a basic test suite for the destinations component.

--- src/app/features/dest/dest.component.ts
+++ src/app/features/dest/dest.component.ts
@@ -3,10 +3,17 @@ import { HeaderComponent } from './header/header.component';
 import { NewsComponent } from './news/news.component';
 import { TailoredComponent } from './tailored/tailored.component';
 import { YourPlanComponent } from './your-plan/your-plan.component';
+import { DestinationsComponent } from './destinations/destinations.component';
 
 @Component({
   selector: 'app-dest',
-  imports: [HeaderComponent, NewsComponent, TailoredComponent, YourPlanComponent],
+  imports: [
+    HeaderComponent,
+    NewsComponent,
+    TailoredComponent,
+    YourPlanComponent,
+    DestinationsComponent,
+  ],
   templateUrl: './dest.component.html',
   styleUrl: './dest.component.css',
 })
---
+++ src/app/features/dest/destinations/destinations.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destinations',
+  imports: [],
+  templateUrl: './destinations.component.html',
+  styleUrl: './destinations.component.css',
+})
+export class DestinationsComponent {}
